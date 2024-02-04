3af5762da893807f5a55f1f6f81fcccd9a630be9
R1nge <r1ngegamedev@gmail.com>
2024-02-04 17:53:32 +0300

    Entry point

---
+++ Assets/_Assets/Scripts/Misc/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using _Assets.Scripts.Services.StateMachine;
+using UnityEngine;
+using VContainer;
+
+namespace _Assets.Scripts.Misc
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        [Inject] private GameStateMachine _gameStateMachine;
+        private void Start() => _gameStateMachine.SwitchState(GameStateType.Game);
+    }
+}
