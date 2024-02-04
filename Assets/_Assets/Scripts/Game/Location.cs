1e4fe0e4f9f910c9bbf6ed2ba83aa908d99df27b
R1nge <r1ngegamedev@gmail.com>
2024-02-04 22:35:50 +0300

    Location rotation

--- Assets/_Assets/Scripts/Game/Location.cs
+++ Assets/_Assets/Scripts/Game/Location.cs
@@ -5,16 +5,16 @@ namespace _Assets.Scripts.Game
     public class Location : MonoBehaviour
     {
         [SerializeField] private float sensitivity = 1.5f;
-        [SerializeField] private float rotationLimitX, rotationLimitY;
+        [SerializeField] private float rotationLimitX, rotationLimitZ;
         private Vector3 _look;
 
         private void Update()
         {
             if (Input.GetMouseButton(0))
             {
-                _look += new Vector3(-Input.GetAxis("Mouse Y") * sensitivity, Input.GetAxis("Mouse X") * sensitivity, 0);
+                _look += new Vector3(Input.GetAxis("Mouse Y") * sensitivity, 0, -Input.GetAxis("Mouse X") * sensitivity);
                 _look.x = Mathf.Clamp(_look.x, -rotationLimitX, rotationLimitX);
-                _look.y = Mathf.Clamp(_look.y, -rotationLimitY, rotationLimitY);
+                _look.z = Mathf.Clamp(_look.z, -rotationLimitZ, rotationLimitZ);
                 transform.eulerAngles = _look;
             }
         }
