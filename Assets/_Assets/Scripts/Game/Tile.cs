7843d2523cadc32cf182638f8d7dd89224288ccc
R1nge <r1ngegamedev@gmail.com>
2024-02-05 14:24:11 +0300

    Tile colliders

---
+++ Assets/_Assets/Scripts/Game/Tile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace _Assets.Scripts.Game
+{
+    public class Tile : MonoBehaviour
+    {
+        [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
+        [SerializeField] private BoxCollider whole;
+        private Vector3 _wholeStartSize;
+
+        private void Awake() => _wholeStartSize = whole.size;
+
+        private void Update()
+        {
+            var blendShapeWeight = 1f - skinnedMeshRenderer.GetBlendShapeWeight(0) / 100f;
+            whole.size = blendShapeWeight <= 0 ? Vector3.zero : _wholeStartSize * blendShapeWeight;
+        }
+    }
+}
